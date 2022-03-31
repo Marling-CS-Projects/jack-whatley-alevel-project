07385853404d84a0ccc9a867b5db8ef920fb0a6c
wheatley1239 <wickselm2005@gmail.com>
2022-03-31 09:49:52 +0100

    experimenting with sprites

--- public/game.js
+++ public/game.js
@@ -1,7 +1,46 @@
 kaboom({
 
-    width: 640,
-    height: 480,
+    width: 1280,
+    height: 720,
     background: [0,0,225],
 
-})
+});
+
+loadSprite("floor", "sprites/floor.png");
+
+const LEVELS = [
+
+    [
+
+        "====================",
+        "====================",
+        "===================="
+
+    ]
+
+]
+
+const levelConf = {
+
+    width: 16,
+    height: 16,
+    pos: vec2(0,0),
+
+    "=": () => [
+
+        sprite("floor"),
+        area(),
+        solid(),
+        origin("bot")
+
+    ]
+
+}
+
+scene("game", (levelNumber = 0) => {
+
+    const level = addLevel(LEVELS[levelNumber], levelConf);
+
+});
+
+go("game");
