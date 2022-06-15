67a535e1da13f6b0ad4bee8155a01fa40002e416
wheatley1239 <wickselm2005@gmail.com>
2022-06-15 10:24:12 +0100

    fixed THREE imports

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.use(express.static("public"));
-app.use(express.static("node_modules"));
+app.use("/THREE", express.static("node_modules/three"));
 
 app.get("/", (req, res) => {
 
