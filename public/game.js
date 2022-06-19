495db617ca09ac90a3baf3871092fef91e7c1d7d
wheatley1239 <wickselm2005@gmail.com>
2022-06-19 12:10:09 +0100

    cleaned up game.js

--- public/game.js
+++ public/game.js
@@ -1,38 +1,41 @@
+// imports:
 import * as THREE from "three";
 import Stats from "./examples/jsm/libs/stats.module.js";
 
 import { OrbitControls, EffectComposer, RenderPass, ShaderPass, GlitchPass, GLTFLoader } from "/exports.js";
 import { createCube, generateCorridor  } from "/exports.js";
 
+// consts:
 const scene = new THREE.Scene();
-scene.background = new THREE.Color(0x87ceeb);
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-
-let moveSpeed = 0.05;
-
-let wKey = false;
-let aKey = false;
-let sKey = false;
-let dKey = false;
-let shKey = false;
-
 const renderer = new THREE.WebGLRenderer();
+const composer = new EffectComposer( renderer );
+const renderPass = new RenderPass( scene, camera );
+const stats = new Stats();
+const sun = new THREE.SpotLight( 0xffffff, 1 );
+const backgroundLight = new THREE.DirectionalLight( 0xffffff, 0.5 );
+const backgroundLight2 = new THREE.DirectionalLight( 0xffffff, 0.5 );
+const controls = new OrbitControls(camera, renderer.domElement);
+const basicCube = createCube([10, 1, 10], 0xffffff);
+const moveableCube = createCube([1, 1, 1], 0xddff00);
+const room = generateCorridor([5, 1, 5], 0xffffff, [10, 0, 0]);
+
+// variables:
+let moveSpeed = 0.05;
+let wKey, aKey, sKey, dKey, shKey;
+
+// body appends:
+document.body.appendChild( stats.dom );
+document.body.appendChild(renderer.domElement);
+
+scene.background = new THREE.Color(0x87ceeb);
+
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.shadowMap.enabled = true;
 renderer.shadowMap.type = THREE.PCFSoftShadowMap;
-document.body.appendChild(renderer.domElement);
 
-const composer = new EffectComposer( renderer );
-const renderPass = new RenderPass( scene, camera );
 composer.addPass( renderPass );
 
-/*(const glitchPass = new GlitchPass();
-composer.addPass( glitchPass );*/
-
-const stats = new Stats();
-document.body.appendChild( stats.dom );
-
-const sun = new THREE.SpotLight( 0xffffff, 1 );
 scene.add(sun);
 sun.position.set(-5, 25, 10);
 sun.lookAt(0,1,0);
@@ -42,19 +45,16 @@ sun.shadow.bias = -0.000001;
 sun.shadow.mapSize.width = 2048 * 4;
 sun.shadow.mapSize.height = 2048 * 4;
 
-const backgroundLight = new THREE.DirectionalLight( 0xffffff, 0.5 );
 scene.add(backgroundLight);
 backgroundLight.castShadow = false;
 backgroundLight.position.set(-5, 5, -5);
 backgroundLight.lookAt(0,1,0);
 
-const backgroundLight2 = new THREE.DirectionalLight( 0xffffff, 0.5 );
 scene.add(backgroundLight2);
 backgroundLight2.castShadow = false;
 backgroundLight2.position.set(5, 5, -5);
 backgroundLight2.lookAt(0,1,0);
 
-const controls = new OrbitControls(camera, renderer.domElement);
 controls.listenToKeyEvents(window);
 
 controls.enableDamping = true;
@@ -67,17 +67,14 @@ controls.maxDistance = 500;
 
 controls.maxPolarAngle = Math.PI / 2;
 
-const basicCube = createCube([10, 1, 10], 0xffffff);
 basicCube.receiveShadow = true;
 basicCube.castShadow = true;
 scene.add(basicCube);
 
-const moveableCube = createCube([1, 1, 1], 0xddff00);
 moveableCube.receiveShadow = true;
 moveableCube.castShadow = true;
 scene.add(moveableCube);
 
-const room = generateCorridor([5, 1, 5], 0xffffff, [10, 0, 0]);
 scene.add(room);
 
 moveableCube.position.set(0, 1, 0);
@@ -89,7 +86,7 @@ camera.position.y = 5;
 
 camera.lookAt(0,1,0);
 
-scene.add( new THREE.AxesHelper(500) );
+scene.add( new THREE.AxesHelper(1000) );
 
 window.addEventListener("keydown", (e) => {
 
@@ -148,6 +145,7 @@ function onWindowResize() {
 }
 
 function animate() {
+    
     requestAnimationFrame( animate );
     
     if (wKey === true) {
@@ -183,7 +181,6 @@ function animate() {
     }
 
     stats.update();
-
     composer.render();
 
 }
