2781d6f7888bd62302cc5856d8da950b60c406bb
jack-whatley <wickselm2005@gmail.com>
2022-08-31 11:49:24 +0100

    working outline texture

--- public/scripts/mapClass.js
+++ public/scripts/mapClass.js
@@ -22,18 +22,27 @@ class Map {
 
     createMapScreen() {
         for (let i = 0; i < this.rooms.length; i++) {
-            let material = new THREE.MeshStandardMaterial({color: 0xffffff});
+            let material = new THREE.MeshStandardMaterial({color: 0x000000});
+            let outlineMaterial = new THREE.MeshBasicMaterial({color: 0xffffff, side: THREE.BackSide});
+
             let component = createCube([this.rooms[i].size[0] - 1, 1, this.rooms[i].size[2] - 1], material);
+            let outline = createCube([this.rooms[i].size[0] - 1, 1, this.rooms[i].size[2] - 1], outlineMaterial);
+
             component.position.set(this.rooms[i].position[0], 1, this.rooms[i].position[2]);
+            outline.position.set(this.rooms[i].position[0], 1, this.rooms[i].position[2]);
+
+            outline.scale.set(1.05,1.05,1.05);
+            
             if (this.rooms[i].constructor.name === "Corridor") {
                 if (this.rooms[i].orientation === "z") {
                     component.rotation.y = degToRad(90);
+                    outline.rotation.y = degToRad(90);
 
                 }
 
             }
 
-            let mapRoom = new MapRoom([component], this.rooms[i]);
+            let mapRoom = new MapRoom([component, outline], this.rooms[i]);
             this.map.push(mapRoom);
 
         }
@@ -43,7 +52,10 @@ class Map {
     createMap(scene) {
         this.createMapScreen();
         this.map.forEach(element => {
-            scene.add(element.components[0]);
+            element.components.forEach(component => {
+                scene.add(component);
+
+            })
 
         })
 
